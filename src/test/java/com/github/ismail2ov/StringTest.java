fd6901b337b445b1add0b385a2e9c4c2315a7b31
Ismail Ahmedov <ismail.ahmedov@asesormasmovil.es>
2023-09-28 00:48:17 +0200

    String strip indent

--- src/test/java/com/github/ismail2ov/StringTest.java
+++ src/test/java/com/github/ismail2ov/StringTest.java
@@ -3,6 +3,7 @@ package com.github.ismail2ov;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.ZonedDateTime;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class StringTest {
@@ -18,5 +19,27 @@ class StringTest {
         String formattedMessage = "User %,d with username %s logged in at %s.".formatted(userId, username, now);
 
         assertThat(formattedMessage).isEqualTo(message);
+    }
+
+    @Test
+    void testStringStripIndent() {
+        String html = """
+              <html>    s
+                <body>      s
+                  <h1>Hello!</h1>
+                </body>    s
+              </html>         s
+            """;
+
+        String expected = html.lines()
+            .map(line -> "|" + line + "|")
+            .collect(Collectors.joining());
+
+        String actual = html.stripIndent()
+            .lines()
+            .map(line -> "|" + line + "|")
+            .collect(Collectors.joining());
+
+        assertThat(actual).isEqualTo(expected);
     }
 }
